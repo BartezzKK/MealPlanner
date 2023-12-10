ca78747dbeea14dc3d7a6c4989cd55e56e8c9800
Bartek <Bartek@BARTEZZKK>
2023-12-10 23:17:03 +0100

    Add project files.

---
+++ MealPlanner/Model/MealPlan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Models
+{
+    class MealPlan
+    {
+        public int Id { get; set; }
+        public DateTime MealDate { get; set; }
+        public int MealId { get; set; }
+        public virtual Meal Meal { get; set; }
+    }
+}
---
+++ MealPlanner/Pages/HomePage.xaml.cs
@@ -0,0 +1,28 @@
+using MealPlanner.Models;
+
+namespace MealPlanner.Pages;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+        mealPlans.ItemsSource = GetMealPlans();
+	}
+
+    private List<MealPlan> GetMealPlans()
+    {
+
+        return new List<MealPlan>
+        {
+            new MealPlan{Id =1, Meal = new Meal{ Id = 1, Name = "Placki", Description="Placki opis" }, MealDate = new DateTime(2023,12,10) },
+            new MealPlan{Id =2, Meal = new Meal{ Id = 2, Name = "Placki2", Description="Placki opis2" }, MealDate = new DateTime(2023,12,11) },
+            new MealPlan{Id =3, Meal = new Meal{ Id = 3, Name = "Placki3", Description="Placki opis3" }, MealDate = new DateTime(2023,12,12) },
+            new MealPlan{Id =4, Meal = new Meal{ Id = 4, Name = "Placki4", Description="Placki opis4" }, MealDate = new DateTime(2023,12,13) },
+            new MealPlan{Id =5, Meal = new Meal{ Id = 5, Name = "Placki5", Description="Placki opis5" }, MealDate = new DateTime(2023,12,14) },
+            new MealPlan{Id =6, Meal = new Meal{ Id = 6, Name = "Placki5", Description="Placki opis5" }, MealDate = new DateTime(2023,12,14) },
+            new MealPlan{Id =7, Meal = new Meal{ Id = 7, Name = "Placki5", Description="Placki opis5" }, MealDate = new DateTime(2023,12,14) }
+        };
+
+    }
+}
